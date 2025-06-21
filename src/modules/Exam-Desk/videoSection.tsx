131f7cc40cfa2190e8e9a8f7906e1f988d900fe0
MorsH14 <149718499+MorsH14@users.noreply.github.com>
2025-06-21 23:15:15 +0100

    videoo resize

--- src/modules/Exam-Desk/videoSection.tsx
+++ src/modules/Exam-Desk/videoSection.tsx
@@ -4,10 +4,14 @@ import styled from "@emotion/styled";
 
 const VideoWrapper = styled.div`
   position: relative;
-  width: 100%;
+  width: 70%;
   /* max-width: 900px; */
   margin: 4rem auto;
   padding-top: 56.25%; /* 16:9 Aspect Ratio */
+
+  @media screen and (max-width: 700px){
+    width: 95%;
+  }
 `;
 
 const Iframe = styled.iframe`
@@ -15,8 +19,12 @@ const Iframe = styled.iframe`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 60%;
   border: none;
+
+  @media screen and (max-width: 700px){
+    height: auto;
+  }
 `;
 
 export default function CodedbusVideo() {
